a7521c3f0b9f5c8c8c68d8964f844bf974c0947c
이가현 <dlrkgus0858@gmail.com>
2025-03-17 01:35:33 +0900

    Title: 이상한 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250317/이상한 수열/a-strange-sequence.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int F(int n)
+{
+    if (n == 1)
+        return 1;
+    else if (n == 2)
+        return 2;
+
+    return F(n / 3) + F(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << F(n);
+
+    return 0;
+}
