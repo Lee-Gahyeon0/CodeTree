e8402c1c29659303675c742dee233f1f4e631d1a
이가현 <dlrkgus0858@gmail.com>
2025-03-18 17:06:05 +0900

    Title: B부터 A까지 감소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250318/B부터 A까지 감소/dec-from-b-to-a.cpp
+++ 250318/B부터 A까지 감소/dec-from-b-to-a.cpp
@@ -7,7 +7,7 @@ int main()
     int a,b;
     cin >> a >> b;
 
-    for (int i = b; i <=a; i--) {
+    for (int i = b; i >=a; i--) {
     cout << i << " " ;
     }
     // Please write your code here.
