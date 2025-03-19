568a486086c10e35de5dcd6b0807a334124cfaa7
이가현 <dlrkgus0858@gmail.com>
2025-03-20 00:03:01 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250320/점수 비교/score-comparison.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a,b,c,d;
+    cin >> a >> b;
+    cin >> c >> d;
+
+    if (a>c){
+        if (b>d){
+            cout << "1";
+        }
+    }
+    else {
+        cout << "0";
+    }
+
+
+
+    return 0;
+}
