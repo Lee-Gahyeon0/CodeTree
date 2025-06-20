14e4bb13fa2f1f4af87e127f36ede60238c48a6f
이가현 <dlrkgus0858@gmail.com>
2025-06-20 14:25:32 +0900

    Title: A부터 B까지 짝수의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250620/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    int c=0;
+
+    cin >> a >> b;
+    for (int i = a, i <=b, i++){
+        if(i%2==0){
+            c +=i;
+        }
+    }
+    cout << i;
+    
+    return 0;
+}
