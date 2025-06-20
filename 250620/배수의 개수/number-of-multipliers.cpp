3062969d4f757cd962aa5b4636e6ab0a50bfaf5b
이가현 <dlrkgus0858@gmail.com>
2025-06-20 14:30:36 +0900

    Title: 배수의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250620/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int total_3 = 0;
+    int total_5 = 0;
+
+    
+    for (int i = 0; i <10; i++){
+        cin >> a;
+        if(a%3==0){
+            total_3 += 1;
+        }
+        else if(a%5==0){
+            total_5 += 1;
+        }
+    }
+    cout << total_3 << " " << totla_5;
+
+    return 0;
+}
