fd432b2d93d0ac6b46f31ff7cc7e0b89fbb56f60
이가현 <dlrkgus0858@gmail.com>
2025-12-22 20:55:34 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251222/두 사람/two-person.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,c;
+    char b,d;
+
+    cin >> a >> b >> c >> d;
+    if((a>=19 && c>=19) ||0 ( b =='M' && d == 'M')){
+        cout << "1";
+    }
+
+    return 0;
+}
