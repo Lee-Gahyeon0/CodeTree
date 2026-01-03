c164c03a4af377adec648cb052cc18eedd90dde6
이가현 <dlrkgus0858@gmail.com>
2026-01-03 21:23:16 +0900

    Title: 배수의 개수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260103/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    int cnt = 0;
+
+    for(int i=0;i<10;i++){
+        cin >> num;
+        if(num%2!=0){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
