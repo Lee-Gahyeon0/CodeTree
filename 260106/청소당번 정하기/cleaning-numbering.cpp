2d02411a9f9a9a543c0f27e0fbd3f4bd1b8c950c
이가현 <dlrkgus0858@gmail.com>
2026-01-06 20:55:09 +0900

    Title: 청소당번 정하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260106/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int clas,hal,ras,n;
+    int cnt1=0;
+    int cnt2=0;
+    int cnt3=0;
+
+    cin >> n;
+     
+    for(int i=1;i<=n;i++){
+        if(i%12==0){
+            cnt3++;
+        }
+        else if(i%3==0){
+            cnt2++;
+        }
+        else if(i%2==0){
+            cnt1++;
+        }
+
+    }
+
+    cout << cnt1 << " " << cnt2 << " " << cnt3;
+
+    return 0;
+}
