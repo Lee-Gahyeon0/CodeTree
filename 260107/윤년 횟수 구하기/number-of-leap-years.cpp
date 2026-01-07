64f628d2b6272c00952b9518bf4525751b0a78e9
이가현 <dlrkgus0858@gmail.com>
2026-01-07 13:32:34 +0900

    Title: 윤년 횟수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt=0;
+    cin  >> n;
+
+    for(int i=1;i<=n;i++){
+        if(i%4==0){
+            if(i%100==0 && i%400!=0){
+                continue;
+            }
+                cnt++;
+        }
+    }
+
+
+    cout << cnt;
+    return 0;
+}
