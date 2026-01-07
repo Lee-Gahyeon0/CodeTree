afbf6c54b6b3fd004651174a237df0560166f917
이가현 <dlrkgus0858@gmail.com>
2026-01-07 15:14:25 +0900

    Title: 등장횟수 확인, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    int cnt=0;
+
+    for(int i=0;i<5;i++){
+        cin >> arr[i];
+        if(arr[i]%2==0){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
