db7547268557d51461b6c86d71d98f1db2f18bfd
이가현 <dlrkgus0858@gmail.com>
2026-01-09 00:32:23 +0900

    Title: 특정 조건의 온전수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260109/특정 조건의 온전수 구하기/print-onjeonsu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt=0;
+    cin >> n;
+
+    for(int i=1;i<=n;i++){
+        if(i%2==0){
+            continue;
+        }
+        if(i%10==5){
+            continue;
+        }
+        if(i%3==0 && i%9!=0){
+            continue;
+        }
+        cout << i << " ";
+    }
+    
+    return 0;
+}
