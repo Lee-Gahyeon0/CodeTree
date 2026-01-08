5c8679ffabfd4a98ad0c0dd874796b09d87638fa
이가현 <dlrkgus0858@gmail.com>
2026-01-09 00:02:59 +0900

    Title: A의 배수들의 곱, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260109/A의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    int prod=1;
+    cin >> a >> b;
+
+    for(int i=a;i<=b;i++){
+        if(i%a==0){
+            prod*=i;
+        }
+
+    }
+    cout << prod;
+    return 0;
+}
