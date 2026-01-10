a559916b732bf6b36d1b935c25116929706ebeb8
이가현 <dlrkgus0858@gmail.com>
2026-01-10 14:41:11 +0900

    Title: 특정 수 맞히기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260110/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while(true){
+        cin >> n;
+        if(n==25){
+            cout << "Good" << endl;
+            break;
+        }
+        if(n<=25){
+            cout << "Higher" << endl;
+        }
+        else if(n>=25){
+            cout << "Lower" << endl;
+        }
+    }
+    return 0;
+}
