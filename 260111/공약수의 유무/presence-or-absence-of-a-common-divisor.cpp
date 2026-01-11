0eade0df93fef6af21a7744eaaf57a9923171b47
이가현 <dlrkgus0858@gmail.com>
2026-01-11 18:20:03 +0900

    Title: 공약수의 유무, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260111/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    bool set=false;
+    cin >> a >> b;
+
+    for(int i=a;i<=b;i++){
+        if(1920%i==0 || 2280%i==0){
+            set=true;
+        }
+    }
+
+    if(set==true){
+            cout << "1";
+        }
+        else{
+            cout << "0";
+        }
+    return 0;
+}
