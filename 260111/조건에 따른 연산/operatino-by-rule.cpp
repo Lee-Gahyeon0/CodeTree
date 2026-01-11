f31bff12518abfba2760e158e8ddd816b6e82ed1
이가현 <dlrkgus0858@gmail.com>
2026-01-11 15:52:02 +0900

    Title: 조건에 따른 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260111/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt = 0;
+
+    cin >> n;
+
+    while(true){
+        if(n>=1000){
+            break;
+        }
+
+        if(n%2==0){
+            n=n*3+1;
+            cnt++;
+        }
+        else{
+            n=n*2+2;
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
