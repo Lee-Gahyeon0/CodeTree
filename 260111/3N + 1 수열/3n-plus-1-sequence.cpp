e72b2a296cc535520533a90599050fbebc1a91fa
이가현 <dlrkgus0858@gmail.com>
2026-01-11 14:35:25 +0900

    Title: 3N + 1 수열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260111/3N + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt=0;
+    cin >> n;
+
+    while(true){
+        if(n==1){
+            cout << cnt;
+            break;
+        }
+
+        if(n%2==0){
+            n=n/2;
+            cnt++;
+        }
+        else{
+            n=n*3+1;
+            cnt++;
+        }
+    }
+
+    
+    return 0;
+}
