52dfd8a88615602aa1984ea489681fdf8a5396c3
이가현 <dlrkgus0858@gmail.com>
2026-01-17 14:59:18 +0900

    Title: 상하로 반복하며 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260117/상하로 반복하며 출력/print-in-up-down-reverse.cpp
+++ 260117/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int main() {
     int n;
+	cin >> n;
 	int sum = n+1;
 
-    cin >> n;
-	
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		int now=i;
 		for (int j = 0; j < n; j++) {
 			cout << now;
