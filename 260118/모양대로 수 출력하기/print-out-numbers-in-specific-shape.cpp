8a53ecd24aba7dab62e426f013cadf15de562a8c
이가현 <dlrkgus0858@gmail.com>
2026-01-18 17:44:48 +0900

    Title: 모양대로 수 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/모양대로 수 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=n;i>0;i--){
+        for(int j=n;j>0;j--){
+            if(j<=i){
+                cout << j << " ";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
