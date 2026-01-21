4db39a3d6d7c8833f2cadad8dafe5ef17ef1354b
이가현 <dlrkgus0858@gmail.com>
2026-01-21 20:47:39 +0900

    Title: 원소 10개의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260121/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int sum=0;
+
+    for(int i=0;i<n;i++){
+        cin >> n;
+        sum+=n;
+    }
+    cout << sum;
+    return 0;
+}
