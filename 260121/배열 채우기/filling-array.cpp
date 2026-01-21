c404ceaf3fa0233c74a8a7db5d06910ba78df83f
이가현 <dlrkgus0858@gmail.com>
2026-01-21 23:51:44 +0900

    Title: 배열 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260121/배열 채우기/filling-array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[10];
+    int cnt=0;
+
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+        if(a[i]==0){
+            break;
+        }
+        cnt++;
+	}
+
+	for (int i = cnt-1; i >= 0; i--) {
+		cout << a[i] << " ";
+	}
+
+	return 0;
+
+}
