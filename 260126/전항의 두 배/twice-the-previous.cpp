363680d5f756cbd29973cdfad0b1104446a3b3c9
이가현 <dlrkgus0858@gmail.com>
2026-01-26 18:14:28 +0900

    Title: 전항의 두 배, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260126/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100];
+    
+    cin >> arr[0] >> arr[1];
+    cout << arr[0] << " " << arr[1] << " " ; 
+    for(int i=2;i<10;i++){
+        arr[i]=arr[i-2]*2+arr[i-1];
+        cout << arr[i] << " ";
+    }
+
+    
+    return 0;
+}
