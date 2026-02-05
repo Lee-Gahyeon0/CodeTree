02812b261d26257c1226ee59fb2394cee2379acd
이가현 <dlrkgus0858@gmail.com>
2026-02-05 11:57:41 +0900

    Title: 정수 직사각형, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260205/정수 직사각형/rectangle-of-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    int arr[100][100];
+
+    cin >> n >> m;
+
+    int num=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            arr[i][j]=num;
+            num+=1;
+
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
