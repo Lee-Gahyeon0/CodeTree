50b1a01070dadd6bcfb9b6644f1be6f0084f0f4c
이가현 <dlrkgus0858@gmail.com>
2026-02-06 23:33:29 +0900

    Title: 지그재그로 정수 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260206/지그재그로 정수 채우기/zigzag-numbering.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    int arr[100][100];
+    int cnt=0;
+
+    cin >> n >> m;
+    
+    for(int j=0;j<m;j++){
+        
+            if(j%2!=0){
+                for(int i=n-1;i>=0;i--){
+                arr[i][j]=cnt++;
+                }
+            }
+            else{
+                for(int i=0;i<n;i++){
+                arr[i][j]=cnt++;
+                }
+            }
+
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
