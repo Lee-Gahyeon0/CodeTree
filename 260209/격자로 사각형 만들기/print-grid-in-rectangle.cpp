29c76826e9d4f925562d3633a24621329747b5c1
이가현 <dlrkgus0858@gmail.com>
2026-02-09 00:59:47 +0900

    Title: 격자로 사각형 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260209/격자로 사각형 만들기/print-grid-in-rectangle.cpp
+++ 260209/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -8,22 +8,22 @@ int main() {
 
     cin >> n;
     
-    for(int j=0;j<5;j++){
+    for(int j=0;j<n;j++){
         arr[0][j]=1;
     }
-    for(int i=0;i<5;i++){
+    for(int i=0;i<n;i++){
         arr[i][0]=1;
     }
 
-    for(int i=1;i<5;i++){
-        for(int j=1;j<5;j++){
+    for(int i=1;i<n;i++){
+        for(int j=1;j<n;j++){
             arr[i][j]=arr[i-1][j]+arr[i][j-1]+arr[i-1][j-1];
         }
     }
 
 
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             cout << arr[i][j] << " ";
         }
         cout << endl;
