1f0c13125aff8b43a8b8cee1af19db6332ebc157
이가현 <dlrkgus0858@gmail.com>
2026-02-09 00:00:14 +0900

    Title: 파스칼의 삼각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260208/파스칼의 삼각형/pascals-triangle.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[15][15];
+    
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        arr[i][0]=1;
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=i;j++){
+            if(j==i){
+                 arr[i][j]=1;
+            }
+            else{
+                arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+            }
+        }
+    }
+
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
