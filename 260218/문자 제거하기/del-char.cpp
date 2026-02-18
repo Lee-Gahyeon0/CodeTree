23b16d69e2421e7812961c6635a6846e541b1983
이가현 <dlrkgus0858@gmail.com>
2026-02-18 22:41:11 +0900

    Title: 문자 제거하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260218/문자 제거하기/del-char.cpp
+++ 260218/문자 제거하기/del-char.cpp
@@ -11,7 +11,7 @@ int main() {
     int l=s.length();
     for(int i=0;i<l-1;i++){
         cin >> a;
-        if(a>s.length()){
+        if(a>=s.length()){
             s.erase(s.length()-1,1);
         }
         else{
