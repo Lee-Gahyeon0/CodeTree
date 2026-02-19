0aac904ab9c8084e72eb96d1fb428d7839b939d5
이가현 <dlrkgus0858@gmail.com>
2026-02-19 17:46:26 +0900

    Title: e 제거하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260219/e 제거하기/e-to-remove.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='e'){
+            s.erase(i,1);
+            cout << s;
+            break;
+        }
+    }
+    return 0;
+}
