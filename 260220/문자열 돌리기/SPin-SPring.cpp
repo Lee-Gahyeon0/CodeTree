1961c184df41de6eb312ecc114704f24bdc7bfe2
이가현 <dlrkgus0858@gmail.com>
2026-02-20 23:55:27 +0900

    Title: 문자열 돌리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260220/문자열 돌리기/SPin-SPring.cpp
+++ 260220/문자열 돌리기/SPin-SPring.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // Please write your code here.
+    string s;
+    cin >> s;
+    int l=s.length();
+
+    cout << s << endl;
+    for(int i=0;i<l;i++){
+        s = s.substr(l-1,1)+s.substr(0,l-1);
+        cout << s << endl;
+    }
     return 0;
 }
